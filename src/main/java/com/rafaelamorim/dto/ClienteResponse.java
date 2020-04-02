439b67a4f6e520919a28afefffe8e468bdab2a30
parreirajr <39635130+parreirajr@users.noreply.github.com>
2020-04-01 23:01:46 -0300

    build: return calculated value saldo

--- src/main/java/com/rafaelamorim/dto/ClienteResponse.java
+++ src/main/java/com/rafaelamorim/dto/ClienteResponse.java
@@ -2,6 +2,8 @@ package com.rafaelamorim.dto;
 
 import java.util.Set;
 
+import org.springframework.data.annotation.Transient;
+
 import lombok.Data;
 
 @Data
@@ -13,5 +15,19 @@ public class ClienteResponse {
 	private String contato;
 	private Set<VendaResponse> vendas;
 	private Set<RecebimentoResponse> recebimentos;
+	@Transient
+	private Float saldo;
+	
+	public Float getSaldo() {
+		Float sumVendas = this.vendas.stream()
+				.map(v -> v.getValor())
+				.reduce((a, b) -> a + b)
+				.get();
+		Float sumRecebimentos = this.recebimentos.stream()
+				.map(r -> r.getValor())
+				.reduce((a, b) -> a + b)
+				.get();
+		return sumRecebimentos - sumVendas;
+	}
 	
 }
