e44a50eb0676cb25bf8096a57d124234f880bd0c
Cleverson Parreira Júnior <parreirajr@gmail.com>
2020-04-29 20:50:11 -0300

    Adequação da classe Usuario de acordo com a documentação

--- src/main/java/com/rafaelamorim/domain/Usuario.java
+++ src/main/java/com/rafaelamorim/domain/Usuario.java
@@ -1,17 +1,25 @@
 package com.rafaelamorim.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "usr_usuario")
 public class Usuario {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "usr_id")
 	private Integer id;
+	
+	@Column(name = "usr_usuario")
 	private String username;
+	
+	@Column(name = "usr_senha")
 	private String senha;
 	
 	public Integer getId() {
